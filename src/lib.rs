f590be78f685899c8f46d0a119fee50b2e89c4ca
viz <viz@airmail.cc>
2019-02-10 22:19:11 +0000

    add license

---
+++ src/lib.rs
@@ -0,0 +1,122 @@
+use std::io::{stdout, stdin, Write};
+
+fn determinant(coxsq: &f64, cox: &f64, cons: &f64) -> f64 {
+    /*
+     * determinant of a quadratic eq is cox^2 - 4*coxsq*const
+     *
+     * finding the determinant is quite important because with
+     * this value, one can determine if the roots are natural
+     * or imaginary.
+     */
+
+    cox.powi(2) - (4.0 * coxsq * cons)
+}
+
+fn get_coeff() -> (f64, f64, f64) {
+    /*
+     * get the coefficents from stdin
+     *
+     * returns a tuple of coefficients and constants
+     * the order goes by this
+     * (coefficient of x^2, coefficient of x, constant)
+     */
+
+    // closure to print string
+    let printstr = |string: &str| {
+        print!("{}", string);
+
+        stdout()
+            .flush()
+            .unwrap();
+    };
+
+    // closure to get input. specifically returns f64
+    let input = |prompt: &str| {
+        let mut num = String::new();
+
+        printstr(prompt);
+
+        // read from stdin
+        stdin()
+            .read_line(&mut num)
+            .expect("can't read from stdin");
+
+        // convert string to f64
+        let num: f64 = num
+            .trim()
+            .parse()
+            .unwrap_or_else(|_| 0.0);
+
+        num
+    };
+
+    // coefficient of x squared
+    let coefxsq: f64 = input(&"enter the coefficient of x squared: ");
+    // coefficient of x
+    let coefx: f64 = input(&"enter the coefficient of x: ");
+    // the constant value
+    let cons: f64 = input(&"enter the constant: ");
+
+    (coefxsq, coefx, cons)
+}
+
+pub fn calculate_root() -> (f64, f64, bool) {
+    /*
+     * calculate the root of the qudratic equation
+     * get coefficients directly from get_coeff
+     *
+     * the returned values are root, root, is_real bool
+     */
+
+    // closure to find the absolute value
+    let abs = |n: &f64| {
+        if *n < 0.0 {
+            return -n;
+        }
+
+        return *n;
+    };
+
+    let mut is_real: bool = true;
+
+    // get the coefficients
+    let coeffs = get_coeff();
+    let cofxsq = coeffs.0;
+    let cofx = coeffs.1;
+    let cons = coeffs.2;
+
+    // calculate the determinant
+    let determinant = determinant(&cofxsq, &cofx, &cons);
+
+    // check if it real
+    if determinant < 0.0 { is_real = false; }
+
+    // take the abs value of determinant
+    let determinant = abs(&determinant);
+
+    // calculate -b/2a
+    let fraction: f64 = -cofx / (2.0 * cofxsq);
+
+    (fraction+determinant.sqrt(), fraction-determinant.sqrt(), is_real)
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn determinant_test() {
+        println!("{}", determinant(&4.0, &7.0, &4.0));
+    }
+
+    //#[test]
+    fn input_test() {
+        get_coeff();
+    }
+
+    #[test]
+    fn root_test() {
+        let kek = calculate_root();
+        println!("{:?}", kek);
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,26 @@
+use qes;
+use std::io::{stdout, Write};
+
+fn main() {
+    // get roots
+    let roots = qes::calculate_root();
+
+    // closure to print i
+    let print = |string: &str| {
+        print!("{}", string);
+        // flush
+        stdout().flush().unwrap();
+    };
+
+    print(&"the roots are ");
+
+    print(&roots.0.to_string()[..]);
+    if ! roots.2 { print(&"i"); }
+
+    print(&" ");
+
+    print(&roots.1.to_string()[..]);
+    if ! roots.2 { print(&"i"); }
+
+    println!();
+}
